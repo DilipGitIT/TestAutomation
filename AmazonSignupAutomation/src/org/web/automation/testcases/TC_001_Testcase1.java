555992799bed76f493ad1f2fbed14e06fac599f8
DilipGitIT <smilesaheaddkp@gmail.com>
2020-07-22 22:44:11 +0100

    Firsttestcommit

---
+++ AmazonSignupAutomation/src/org/web/automation/testcases/TC_001_Testcase1.java
@@ -0,0 +1,18 @@
+package org.web.automation.testcases;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class TC_001_Testcase1 {
+	ChromeDriver driver;
+@Test
+	public void startBrowser(){
+		System.setProperty("webdriver.chrome.driver","./Drivers/chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.get("https://www.amazon.co.uk/ap/signin?openid.pape.max_auth_age=0&openid.return_to=https%3A%2F%2Fwww.amazon.co.uk%2Fb%2F%3Fie%3DUTF8%26node%3D18982412031%26ref_%3Dnav_signin&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.assoc_handle=gbflex&openid.mode=checkid_setup&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0&");
+		driver.findElement(By.name("email")).sendKeys("TestUser@gmail.com");
+		driver.close();
+	}
+
+}
